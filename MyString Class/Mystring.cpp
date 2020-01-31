2173252640950b5ae9fdf45cfd593cfb3950df37
Rishi K <59014665+CODESofRishi@users.noreply.github.com>
2020-01-31 22:33:18 +0530

    Update Mystring.cpp

--- MyString Class/Mystring.cpp
+++ MyString Class/Mystring.cpp
@@ -53,7 +53,8 @@ Mystring& Mystring::operator=(const Mystring &rhs) {
 
 // move assignment
 Mystring& Mystring::operator=(const Mystring &&rhs) {
-    if (this == &rhs) return *this;
+    if (this == &rhs)
+        return *this;
     delete [] str;
     str = new char[std::strlen(rhs.str) + 1];
     std::strcpy(str, rhs.str);
